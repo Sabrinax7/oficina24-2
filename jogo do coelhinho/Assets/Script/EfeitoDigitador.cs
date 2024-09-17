bc15ac1d45ecf37933578aae9500107988b328f1
Sabrinax7 <145375466+Sabrinax7@users.noreply.github.com>
2024-09-17 08:13:28 -0300

    commit813

--- jogo do coelhinho/Assets/Script/EfeitoDigitador.cs
+++ jogo do coelhinho/Assets/Script/EfeitoDigitador.cs
@@ -11,29 +11,49 @@ public class EfeitoDigitador : MonoBehaviour
     private AudioSource _audioSource;
     private string mensagemOriginal;
     public bool imprimindo;
+    public float tempoEntreLetras = 0.08f;
 
     private void Aweke()
     {
-
+        TryGetComponent(out componenteTexto);
+        TryGetComponent(out _audioSource);
+        mensagemOriginal = componenteTexto.text;
+        componenteTexto.text = "";
     }
 
     private void OnEnable()
     {
-
+        ImprimirMensagem(mensagemOriginal);
     }
 
     private void OnDisable()
     {
-
+       componenteTexto.text = mensagemOriginal;
+       StopAllCoroutines();
     }
 
-    public ImprimirMensagem(string msg)
+    public void ImprimirMensagem(string mensagem)
     {
-
+        if(gameObject.activeInHierarchy)
+        {
+            if(imprimindo) return;
+            imprimindo = true;
+            StartCoroutine(LetraPorLetra(mensagem));
+        }
     }
 
-    IEnumerator LetraPorLetra()
+    IEnumerator LetraPorLetra(string mensagem)
     {
-        
+        string msg = "";
+        foreach (var letra in mensagem)
+        {
+            msg += letra;
+            componenteTexto.text = msg;
+            _audioSource.Play();
+            yield return new WaitForSeconds(tempoEntreLetras);
+        }
+
+        imprimindo = false;
+        StopAllCoroutines();
     }
 }
